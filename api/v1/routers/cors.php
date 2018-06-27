f02e3bb5a46ee0b3ed1966c25ae7059aca9e4e4e
Camila Bernardon <camilabernardon.es@gmail.com>
2018-06-27 00:07:31 -0400

    add router cors - enable cors in remote provider -- and controlle version with vendor folder :)

---
+++ api/v1/routers/cors.php
@@ -0,0 +1,34 @@
+<?php
+// This Slim setting is required for the middleware to work
+$app = new Slim\App([
+    "settings"  => [
+        "determineRouteBeforeAppMiddleware" => true,
+    ]
+]);
+
+// This is the middleware
+// It will add the Access-Control-Allow-Methods header to every request
+
+$app->add(function($request, $response, $next) {
+    $route = $request->getAttribute("route");
+
+    $methods = [];
+
+    if (!empty($route)) {
+        $pattern = $route->getPattern();
+
+        foreach ($this->router->getRoutes() as $route) {
+            if ($pattern === $route->getPattern()) {
+                $methods = array_merge_recursive($methods, $route->getMethods());
+            }
+        }
+        //Methods holds all of the HTTP Verbs that a particular route handles.
+    } else {
+        $methods[] = $request->getMethod();
+    }
+
+    $response = $next($request, $response);
+
+
+    return $response->withHeader("Access-Control-Allow-Methods", implode(",", $methods));
+});

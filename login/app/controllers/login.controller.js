7ae7494538061763429a96802f3e5e939c73e090
Christian Bacry <chrisbacry@gmail.com>
2018-06-28 01:13:26 -0400

    Login (correcao de CPF)

--- login/app/controllers/login.controller.js
+++ login/app/controllers/login.controller.js
@@ -14,7 +14,7 @@ function LoginController($http) {
 
         $http({
             method: 'POST',
-            url: '../api/v1/usuarios/login/' + lc['cpf'] + '/' + lc['senha'],
+            url: '../api/v1/usuarios/login/' + lc['cpf'].replace('.', '') + '/' + lc['senha'],
         }).then(function (dados) {
 
             console.log(dados['data']);
